110247a699c8e06d16c6c2b903cc8c486df12264
Miguel Rodriguez <miguel.a.rodriguez.r@gmail.com>
2024-06-09 22:01:37 -0500

    logs

--- src/controllers/variantController.js
+++ src/controllers/variantController.js
@@ -2,9 +2,11 @@ const variantService = require('../services/variantService');
 
 exports.createVariant = async (req, res) => {
   try {
+    console.log("body", JSON.stringify(req.body))
     const variant = await variantService.createVariant(req.body);
     res.status(201).json(variant);
   } catch (error) {
+    console.log("error", JSON.stringify(error))
     res.status(500).json({ message: error.message });
   }
 };
